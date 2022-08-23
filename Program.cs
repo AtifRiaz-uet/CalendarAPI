0d63e0c551ed2d67b7acb396a6bb309c057fe197
AtifRiaz-uet <109757936+AtifRiaz-uet@users.noreply.github.com>
2022-08-24 01:47:36 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+
+namespace GettingHolidays
+{
+    class Program
+    {
+        const string apiKey = "AIzaSyCkHEq9efc73mgl0k3Ib7wwI54Gle5hX3M";
+        const string calendarID = "en-gb.pk#holiday@group.v.calendar.google.com";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Just Checking");
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                ApiKey = apiKey,
+                ApplicationName = "API key Example"
+            });
+            var request = service.Events.List(calendarID);
+            request.Fields = "items(summary,start,end)";
+            var response = await request.ExecuteAsync();
+            foreach (var item in response.Items)
+            {
+                Console.WriteLine($"Holiday: {item.Summary} start: {item.Start.Date} End: {item.End.Date}");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
